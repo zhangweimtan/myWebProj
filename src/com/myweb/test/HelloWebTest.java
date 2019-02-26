498aac0fe3616123b6d2d7cb59245c111f44ede5
zhangweimtan <zhangweim@hotmail.com>
2019-02-26 10:24:34 +0800

    by zhangweim 2019-0226

---
+++ src/com/myweb/test/HelloWebTest.java
@@ -0,0 +1,26 @@
+package com.myweb.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWebTest extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doGet(req, resp);
+		System.out.println("this is get...method");
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doPost(req, resp);
+		System.out.println("this is post...method");
+	}
+}
